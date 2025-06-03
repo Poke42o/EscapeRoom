af6250d41043b487f9be33fc1b01e062d84ff894
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-06-03 11:24:03 -0500

    all the dates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,119 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Escape Room");
+        System.out.println("=========================\n");
+
+
+        char genero;
+        char letraExamen;
+        char caracterEspecial;
+        char teclaPulsada;
+
+
+
+
+
+
+        byte EdadPersona;
+        byte velocidadAuto;
+        byte numeroEstrellasCalificacion;
+        byte porcentajeBateria;
+        byte numeroPequeño;
+        byte cantidadHijos;
+
+
+
+
+
+
+        String matriculaVehiculo;
+        String numerotelefono;
+        String codigoPostal;
+        String guardarPalabra;
+        String nombreCompletoPersona;
+        String codigoDeBarras;
+        String numeroMatricula;
+        String idUnico;
+        String nombreUsuarioApp;
+        String codigoNoNumerico;
+        String nombreEmpresa;
+
+
+
+
+
+
+
+
+
+        int numeroTelefono;
+        int productoSinDecimales;
+        int precioBilleteAvion;
+        int puntuacionVideojuego;
+        int cantidadVisitantesSitioWeb;
+
+
+
+        long poblacionMundial;
+
+
+
+
+
+        float  alturaPersonaMetros;
+        float  precioProducto;
+        float SalarioTrabajador;
+        float puntajeExamen;
+        float temperaturaAmbiente;
+        float calificacionPromedio;
+        float cambioDivisas;
+        float puntajePromedio;
+        float volumenRecipiente;
+        float notaFinal;
+
+
+
+
+
+
+
+        short cantidadProductos;
+        short cantidadLibros;
+        short pesoPersona;
+
+
+
+
+        double distanciaKilometros;
+
+
+        boolean estaEncendido=false;
+        boolean esMayorDeEdad=false;
+        boolean puertaAbierta=false;
+        boolean archivoActivo=false;
+        boolean tieneLicencia=false;
+        boolean elSensorFunciona=false;
+        boolean estaRegistrado=false;
+        boolean pagoConfirmado=false;
+
+
+
+
+
+
+
+        LocalDate fechaNacimiento;
+        LocalTime horaExacta;
+
+
+
+
+
+
     }
 }
